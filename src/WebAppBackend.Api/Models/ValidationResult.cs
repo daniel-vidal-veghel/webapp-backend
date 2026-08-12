c152cda1b3b1e56e27ebacb28f56651707a1a3e6
daniel-vidal-veghel <248000443+daniel-vidal-veghel@users.noreply.github.com>
2026-08-13 00:06:55 +0200

    ValidationResult => IEquatable.

--- src/WebAppBackend.Api/Models/ValidationResult.cs
+++ src/WebAppBackend.Api/Models/ValidationResult.cs
@@ -4,7 +4,7 @@ namespace WebAppBackend.Api.Models;
 /// </summary>
 /// <remarks> ContentSections are meant to flexible, today and in the future. That's why there's a validation process.<br/>
 /// ValidationResults, on the other hand, are hard coded ad-hoc. To prevent accidental bugs, all fields are required.</remarks>
-public class ValidationResult : ContentSection
+public class ValidationResult : ContentSection, IEquatable<ValidationResult>
 {
 #pragma warning disable CS8765 // strings are nullable in baseclass, but required in ValidationResult
 	public override required string Id { get; set; }
@@ -13,4 +13,19 @@ public class ValidationResult : ContentSection
 	public override required string Title { get; set; }
 	public override required string Html { get; set; }
 #pragma warning restore CS8765
+	
+	public bool Equals(ValidationResult? other) => this == other;                               // Required by IEquatable
+	public override bool Equals(object? obj) => this == (obj as ValidationResult);              // CS0660
+	public override int GetHashCode() => HashCode.Combine(Id, Title, Description, Html, Type);  // CS0661
+	public static bool operator !=(ValidationResult? left, ValidationResult? right) => !(left == right);
+	public static bool operator ==(ValidationResult? left, ValidationResult? right)
+	{
+		if (ReferenceEquals(left, right)) return true;
+		if (left is null || right is null) return false;
+		return left.Id == right.Id
+			&& left.Title == right.Title
+			&& left.Description == right.Description
+			&& left.Html == right.Html
+			&& left.Type == right.Type;
+	}
 }
